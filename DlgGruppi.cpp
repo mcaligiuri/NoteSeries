4f2333464f8f7d9ca8b0a1aceed2f39d65b696ce
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2019-03-28 03:39:55 +0100

    Definita funzione SetEtichetta(). Creo colonne e popolo combobox delle etichette

--- DlgGruppi.cpp
+++ DlgGruppi.cpp
@@ -17,8 +17,8 @@ DlgGruppi::DlgGruppi(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_GRUPPI, pParent)
 {
 	m_temp = _T("");
+	m_sql = _T("");
 	m_rows = 0;
-
 }
 
 DlgGruppi::~DlgGruppi()
@@ -40,6 +40,7 @@ void DlgGruppi::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(DlgGruppi, CDialogEx)
+	ON_BN_CLICKED(IDC_BTNADD, &DlgGruppi::SetEtichetta)
 END_MESSAGE_MAP()
 
 
@@ -57,8 +58,46 @@ BOOL DlgGruppi::OnInitDialog()
 		m_rows = m_lstNomi.InsertItem(i, _T(""));
 		m_lstNomi.SetItemText(m_rows, 0, pDoc->m_griglia[i].nome);
 	}
-
-
 	m_lstAss.InsertColumn(0, m_currlang.GetDesc(38, m_temp), LVCFMT_CENTER, 150);
+
+	// Popolo combobx delle etichette
+	m_nomi.RemoveAll();
+	dbconfig.GetGruppi(&m_nomi);
+	for (int j = 0; j < m_nomi.GetCount(); j++)
+		m_cmbLabel.AddString(m_nomi.GetAt(j));
+	
+	m_cmbLabel.SetCurSel(0);
 	return TRUE;
+}
+
+
+// Aggiungo un'etichetta
+void DlgGruppi::SetEtichetta()
+{
+	CString lbl, buf = _T("");	
+	m_txtAdd.GetWindowTextW(lbl);
+
+	// Caratteri speciali sostituiti con uno spazio
+	char specialChar[10] = "\"\\/:*|<>";
+	for (short i = 0; i < 10; i++)
+	{
+		buf.Format(_T("%c"), specialChar[i]);
+		if (lbl.Find(buf) != -1 && lbl.Find(buf) != '\0')
+			lbl.Replace(buf, _T(" "));
+	}
+	// Controllo spazi ad inizio e fine
+	int start = lbl.Find(L" ");
+	if (start == 0)
+		lbl.Delete(start, 1);
+
+	int end = lbl.Find(L" ");
+	if (end == lbl.GetLength() - 1)
+		lbl.Delete(end, lbl.GetLength() - 1);
+
+	m_sql.Format(_T("INSERT INTO [ETICHETTE](NOME) VALUES('%s')"),lbl);
+	
+	if (!dbconfig.SetSerie(m_sql))
+		return;
+	
+	m_nomi.Add(lbl);
 }
