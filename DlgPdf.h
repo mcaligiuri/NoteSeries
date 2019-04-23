ec0372a38916fa6678ad4302da457939baef31ba
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2019-04-23 04:04:29 +0200

    Aggiunte variabili per chiavi di registro e per salvare la griglia. Aggiunta dichiarazione per le  traduzioni e variabili associate ai controlli grafici.

--- DlgPdf.h
+++ DlgPdf.h
@@ -19,10 +19,39 @@ public:
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // supporto DDX/DDV
+	// Variabili salvataggio griglia
+	CString m_nome;
+	CString m_cartella;
+	CString m_inizio;
+	CString m_fine;
+	CString m_stato;
+	CString m_priorità;
+	CString m_sito;
+	CString m_voto;
+	CString m_commento;
+	// Chiavi di registro
+	CRegKey chiave;
+	ULONG len;
+	CString retLang;
+	CString retPath;
 
 	DECLARE_MESSAGE_MAP()
 public:
 	virtual BOOL OnInitDialog();
+	void GetDescUpdate();
 	CNoteSeriesDoc* GetDoc();
 	CNoteSeriesDoc* pDoc;
+	CString m_temp;
+private:
+	CStatic m_lblTitle;	// Titolo finestra
+	CButton m_btnCrea;	// Bottone che crea pdf
+	CButton m_btnCanc;	// Chiude la finestra
+public:
+	afx_msg void Annulla();
+	afx_msg void SetPdf();
+private:
+	// Etichetta nome
+	CStatic m_lblNome;
+	// Casella di testo per inserire il nome del pdf
+	CEdit m_txtNome;
 };
